85a739c081c7aca98225e5fcdb1cc6df261a1886
wtretter <will@wtretter.com>
2025-11-23 13:33:14 -0600

    fixed typos

--- server-update.py
+++ server-update.py
@@ -9,23 +9,23 @@ args = parser.parse_args()
 
 branch = args.branch.split("/")[-1]
 
-subprocess.run("docker kill space-game-web-frontend-{branch}")
-subprocess.run("docker kill space-game-api-{branch}")
-subprocess.run("docker kill space-game-db-{branch}")
+subprocess.run(["docker", "kill", f"space-game-web-frontend-{branch}"])
+subprocess.run(["docker", "kill", f"space-game-api-{branch}"])
+subprocess.run(["docker", "kill", f"space-game-db-{branch}"])
 
-subprocess.run("docker rm space-game-web-frontend-{branch}")
-subprocess.run("docker rm space-game-api-{branch}")
-subprocess.run("docker rm space-game-db-{branch}")
+subprocess.run(["docker", "rm", f"space-game-web-frontend-{branch}"])
+subprocess.run(["docker", "rm", f"space-game-api-{branch}"])
+subprocess.run(["docker", "rm", f"space-game-db-{branch}"])
 
-subprocess.run("docker network rm space-game-{branch}")
+subprocess.run(["docker", "network", "rm", f"space-game-{branch}"])
 
-subprocess.run("docker network create space-game-{branch}")
+subprocess.run(["docker", "network", "create", f"space-game-{branch}"])
 
-subprocess.run("docker build -f Dockerfile -t space-game-api-{branch}")
+subprocess.run(["docker", "build", "-f", "Dockerfile", "-t", f"space-game-api-{branch}"])
 
-subprocess.run("docker run --name space-game-db-{branch} --network space-game-{branch} mongo:latest")
-subprocess.run("docker run --name space-game-api-{branch} --network space-game-{branch} space-game-api-{branch}")
-subprocess.run("docker run --name space-game-web-frontend-{branch} --network space-game-{branch} -v ./web-frontend:/usr/share/nginx/html -v ./nginx.conf:/etc/nginx/conf.d/default.conf -P nginx")
+subprocess.run(["docker", "run", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "mongo:latest"])
+subprocess.run(["docker", "run", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", f"space-game-api-{branch}"])
+subprocess.run(["docker", "run", "--name", f"space-game-web-frontend-{branch}", "--network", f"space-game-{branch}", "-v", "./web-frontend:/usr/share/nginx/html", "-v", "./nginx.conf:/etc/nginx/conf.d/default.conf", "-P", "nginx"])
 
-port = subprocess.run("docker port space-game-web-frontend-{branch}", capture_output=True)
+port = subprocess.run(["docker", "port", f"space-game-web-frontend-{branch}"], capture_output=True)
 print(port)
