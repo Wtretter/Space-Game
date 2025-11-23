bff480b30fb20d5938699bd25946dba7e7c31608
wtretter <will@wtretter.com>
2025-11-23 13:46:15 -0600

    added flush=True to debug prints

--- server-update.py
+++ server-update.py
@@ -9,30 +9,30 @@ args = parser.parse_args()
 
 branch = args.branch.split("/")[-1]
 
-print("[!]killing old containers")
+print("[!]killing old containers", flush=True)
 subprocess.run(["docker", "kill", f"space-game-web-frontend-{branch}"])
 subprocess.run(["docker", "kill", f"space-game-api-{branch}"])
 subprocess.run(["docker", "kill", f"space-game-db-{branch}"])
 
-print("[!]removing old containers")
+print("[!]removing old containers", flush=True)
 subprocess.run(["docker", "rm", f"space-game-web-frontend-{branch}"])
 subprocess.run(["docker", "rm", f"space-game-api-{branch}"])
 subprocess.run(["docker", "rm", f"space-game-db-{branch}"])
 
-print("[!]removing old network")
+print("[!]removing old network", flush=True)
 subprocess.run(["docker", "network", "rm", f"space-game-{branch}"])
 
-print("[!]creating new network")
+print("[!]creating new network", flush=True)
 subprocess.run(["docker", "network", "create", f"space-game-{branch}"])
 
-print("[!]building new image")
+print("[!]building new image", flush=True)
 subprocess.run(["docker", "build", "-f", "Dockerfile", "-t", f"space-game-api-{branch}", "."])
 
-print("[!]running containers")
+print("[!]running containers", flush=True)
 subprocess.run(["docker", "run", "-d", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "mongo:latest"])
 subprocess.run(["docker", "run", "-d", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", f"space-game-api-{branch}"])
 subprocess.run(["docker", "run", "-d", "--name", f"space-game-web-frontend-{branch}", "--network", f"space-game-{branch}", "-v", "./web-frontend:/usr/share/nginx/html", "-v", "./nginx.conf:/etc/nginx/conf.d/default.conf", "-P", "nginx"])
 
-print("[!]pulling random port")
+print("[!]pulling random port", flush=True)
 port = subprocess.run(["docker", "port", f"space-game-web-frontend-{branch}"], capture_output=True)
-print(port.stdout)
+print(port.stdout, flush=True)
