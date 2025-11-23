870488cd0536b43df2e205f0c6eb6faf869a2b82
wtretter <will@wtretter.com>
2025-11-23 13:51:30 -0600

    added hostnames to docker containers

--- server-update.py
+++ server-update.py
@@ -29,8 +29,8 @@ print("[!]building new image", flush=True)
 subprocess.run(["docker", "build", "-f", "Dockerfile", "-t", f"space-game-api-{branch}", "."])
 
 print("[!]running containers", flush=True)
-subprocess.run(["docker", "run", "-d", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "mongo:latest"])
-subprocess.run(["docker", "run", "-d", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", f"space-game-api-{branch}"])
+subprocess.run(["docker", "run", "-d", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "--hostname", "db", "mongo:latest"])
+subprocess.run(["docker", "run", "-d", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", "--hostname", "api", f"space-game-api-{branch}"])
 subprocess.run(["docker", "run", "-d", "--name", f"space-game-web-frontend-{branch}", "--network", f"space-game-{branch}", "-v", "./web-frontend:/usr/share/nginx/html", "-v", "./nginx.conf:/etc/nginx/conf.d/default.conf", "-P", "nginx"])
 
 print("[!]pulling random port", flush=True)
