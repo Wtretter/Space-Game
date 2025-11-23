99d7f41db67c2c0648f4c08b13f16cfcee19748b
wtretter <will@wtretter.com>
2025-11-23 13:36:35 -0600

    added demons

--- server-update.py
+++ server-update.py
@@ -23,9 +23,9 @@ subprocess.run(["docker", "network", "create", f"space-game-{branch}"])
 
 subprocess.run(["docker", "build", "-f", "Dockerfile", "-t", f"space-game-api-{branch}"])
 
-subprocess.run(["docker", "run", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "mongo:latest"])
-subprocess.run(["docker", "run", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", f"space-game-api-{branch}"])
-subprocess.run(["docker", "run", "--name", f"space-game-web-frontend-{branch}", "--network", f"space-game-{branch}", "-v", "./web-frontend:/usr/share/nginx/html", "-v", "./nginx.conf:/etc/nginx/conf.d/default.conf", "-P", "nginx"])
+subprocess.run(["docker", "run", "-d", "--name", f"space-game-db-{branch}", "--network", f"space-game-{branch}", "mongo:latest"])
+subprocess.run(["docker", "run", "-d", "--name", f"space-game-api-{branch}", "--network", f"space-game-{branch}", f"space-game-api-{branch}"])
+subprocess.run(["docker", "run", "-d", "--name", f"space-game-web-frontend-{branch}", "--network", f"space-game-{branch}", "-v", "./web-frontend:/usr/share/nginx/html", "-v", "./nginx.conf:/etc/nginx/conf.d/default.conf", "-P", "nginx"])
 
 port = subprocess.run(["docker", "port", f"space-game-web-frontend-{branch}"], capture_output=True)
 print(port)
